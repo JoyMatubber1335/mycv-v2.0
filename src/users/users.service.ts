dc02b69ee9a79edecbef4f7ec364cd757fe19217
JoyMatubber1335 <joy.matubber@brainstation-23.com>
2024-10-08 19:53:46 +0600

    Complete CRUD opetarion

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -10,24 +10,25 @@ export class UsersService {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
-  findOne(id: number) {
-    const user = this.repo.findOneBy({ id });
+  async findOne(id: number) {
+    const user = await this.repo.findOneBy({ id });
+    console.log(user);
     if (!user) {
-      console.log('user not found');
+      throw new NotFoundException('user not found');
     }
     return user;
   }
   find(email: string) {
     const user = this.repo.find({ where: { email } });
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     return user;
   }
   async update(id: number, attars: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     Object.assign(user, attars);
     return this.repo.save(user);
@@ -35,7 +36,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     return this.repo.remove(user);
   }
